8980de6dbf2141cee8a2a3fdde9379b918e6bde6
kidalonious <120538119+kidalonious@users.noreply.github.com>
2025-06-09 21:14:42 -0600

    removed a large comment that was being used to understand the chess library

--- pgnHandler.go
+++ pgnHandler.go
@@ -7,21 +7,6 @@ import (
 
 	"github.com/notnil/chess"
 )
-// from the notnil/chess documentation on scanning a large pgn file:
-/* 
-	f, err := os.Open("lichess_db_standard_rated_2013-01.pgn")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	scanner := chess.NewScanner(f)
-	for scanner.Scan() {
-		game := scanner.Next()
-		fmt.Println(game.GetTagPair("Site"))
-		// Output &{Site https://lichess.org/8jb5kiqw}
-	}
-*/
 
 type Pgn struct {
 	Headers map[string]string
